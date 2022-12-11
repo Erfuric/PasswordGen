8323f8ea9e12645d73049b6fe8216add8bd03be3
Callum Mackintosh <callum.e.s.mackintosh@gmail.com>
2022-12-11 19:18:29 +1100

    Committing work, password length not working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,70 +1,62 @@
-// Assignment Code
+// Password generator
+
 var generateBtn = document.querySelector("#generate");
-
-
 
 // Write password to the #password input
 function generatePassword() {
 
-// variable group for password complexity
-var special = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+// arrays for character groups
+var special = "!@#$%^&*()_+-=[]{}|;':\"<>,.?/~`";
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowercase = "abcdefghijklmnopqrstuvwxyz";
 var numeric = "0123456789";
 
-// Defining password as empty string
-var password = "";
-
-// Prompts for which character types to include
-
+// Prompts define which character types to include
 var includelowercase = confirm("Include lowercase characters in password?");
 var includeuppercase = confirm("Include uppercase characters in password?");
 var includenumeric = confirm("Include numberical characters in password?");
 var includespecial = confirm("Include special characters in password?");
 
 // Creating an array that stores user selections
-var passwordvars = [];
-if (includelowercase) { passwordvars = passwordvars.concat(lowercase);}
-if (includeuppercase) { passwordvars = passwordvars.concat(uppercase);}
-if (includenumeric) { passwordvars = passwordvars.concat(numeric);}
-if (includespecial) { passwordvars = passwordvars.concat(special);}
+var passwordChars = [];
+if (includelowercase) { passwordChars = passwordChars.concat(lowercase); }
+if (includeuppercase) { passwordChars = passwordChars.concat(uppercase); }
+if (includenumeric) { passwordChars = passwordChars.concat(numeric); }
+if (includespecial) { passwordChars = passwordChars.concat(special); }
 
 // Alert if no options were selected
-if (passwordvars.length === 0) {
+if (passwordChars.length === 0) {
   alert("You must select at least one character option.");
   return;
 }
 
 // Password length prompt
-var PasswordLength = prompt("Enter a password length between 12 and 128");
+var passwordLength = prompt("Enter a password length between 12 and 32");
 // Validating that input is within specified range
-if (isNaN(PasswordLength) || PasswordLength < 12 || PasswordLength > 128) {
-  alert("Invalid input, please enter a number between 12 and 128");
+if (isNaN(passwordLength) || passwordLength < 12 || passwordLength > 32) {
+  alert("Invalid input, please enter a number between 12 and 32");
   return;
 }
 
 // Password generation with for loop
-for (var i = 0; i < PasswordLength; i++) {
-  var randomIndex = Math.floor(Math.random() * passwordvars.length);
-  var randomPass = passwordvars[randomIndex];
 
-// Adding result to password variable
-  password += randomPass;
+for (var i = 0; i < passwordLength; i++) {
+  var randomIndex = Math.floor(Math.random() * passwordChars.length);
+  var randomChar = passwordChars[randomIndex];
+  password += randomChar;
 }
 
-return password;
+var passwordInput = document.querySelector("#password");
+passwordInput.value = password;
 
+var confirmPassword = confirm("Your password is: " + password + " Click OK to use this password, or click Cancel to generate a new password.");
+
+  if (confirmPassword) {
+    return password;
+  } else {
+    return generatePassword();
+  }
 }
-
-
-// var password = writePassword();
-// var passwordText = document.querySelector("#password");
-
-//  passwordText.value = password;
-
-
-
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", generatePassword);
